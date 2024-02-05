827a700b15cb8d419d4b620c0a8dff7827002510
Daniaz <Dania@ideo-digital.com>
2024-02-05 21:48:46 +0200

    Add project files.

---
+++ Cyberbit.TaskManager.Server/Models/Category.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cyberbit.TaskManager.Server.Models
+{
+    public class Category
+    {
+        [Key, Required]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public virtual List<Task> Tasks { get; set; }
+
+    }
+}
